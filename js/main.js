5498ba48b30d7a0174623073b076b7db9aaa7b79
gonnavis <gonnavis@163.com>
2019-07-08 17:30:57 +0800

    a

--- js/main.js
+++ js/main.js
@@ -6,14 +6,8 @@ var isStarted = false;
 var localStream;
 var pc;
 var remoteStream;
-var maxBandwidth=64
+var maxBandwidth = 64
 var isControllee = location.href.indexOf('controllee') >= 0
- var mediaConstraints = {
-   'mandatory': {
-     'OfferToReceiveAudio': true,
-     'OfferToReceiveVideo': true
-   }
- };
 
 var pcConfig = {
   'iceServers': [{
@@ -77,14 +71,14 @@ socket.on('message', function (message) {
     if (!isInitiator && !isStarted) {
       maybeStart();
     }
-    let sessionDescription=new RTCSessionDescription(message)
+    let sessionDescription = new RTCSessionDescription(message)
     pc.setRemoteDescription({
       type: sessionDescription.type,
       sdp: updateBandwidthRestriction(sessionDescription.sdp, maxBandwidth)
     });
     doAnswer();
   } else if (message.type === 'answer' && isStarted) {
-    let sessionDescription= new RTCSessionDescription(message)
+    let sessionDescription = new RTCSessionDescription(message)
     pc.setRemoteDescription({
       type: sessionDescription.type,
       sdp: updateBandwidthRestriction(sessionDescription.sdp, maxBandwidth)
@@ -205,24 +199,28 @@ function handleCreateOfferError(event) {
 
 function doCall() {
   console.log('Sending offer to peer');
-  pc.createOffer(mediaConstraints).then(
-    setLocalAndSendMessage,
-    onCreateSessionDescriptionError
-  );
+  pc.createOffer(setLocalAndSendMessage, handleCreateOfferError, {
+    offerToReceiveAudio: 0,
+    offerToReceiveVideo: 0
+  });
 }
 
 function doAnswer() {
   console.log('Sending answer to peer.');
-  pc.createAnswer(mediaConstraints).then(
+  pc.createAnswer({
+    offerToReceiveAudio: 0,
+    offerToReceiveVideo: 1
+  }).then(
     setLocalAndSendMessage,
     onCreateSessionDescriptionError
   );
 }
 
 function setLocalAndSendMessage(sessionDescription) {
-  // Set Opus as the preferred codec in SDP if Opus is present.
-  sessionDescription.sdp = preferOpus(sessionDescription.sdp);
-  pc.setLocalDescription(sessionDescription);
+  pc.setLocalDescription({
+    type: sessionDescription.type,
+    sdp: updateBandwidthRestriction(sessionDescription.sdp, maxBandwidth)
+  });
   console.log('setLocalAndSendMessage sending message', sessionDescription);
   sendMessage(sessionDescription);
 }
@@ -275,74 +273,3 @@ function updateBandwidthRestriction(sdp, bandwidth) {
   }
   return sdp;
 }
-
-  // Set Opus as the default audio codec if it's present.
-  function preferOpus(sdp) {
-    var sdpLines = sdp.split('\r\n');
-
-    // Search for m line.
-    for (var i = 0; i < sdpLines.length; i++) {
-      if (sdpLines[i].search('m=audio') !== -1) {
-        var mLineIndex = i;
-        break;
-      }
-    }
-    if (mLineIndex === null)
-      return sdp;
-
-    // If Opus is available, set it as the default in m line.
-    for (var i = 0; i < sdpLines.length; i++) {
-      if (sdpLines[i].search('opus/48000') !== -1) {
-        var opusPayload = extractSdp(sdpLines[i], /:(\d+) opus\/48000/i);
-        if (opusPayload)
-          sdpLines[mLineIndex] = setDefaultCodec(sdpLines[mLineIndex], opusPayload);
-        break;
-      }
-    }
-
-    // Remove CN in m line and sdp.
-    sdpLines = removeCN(sdpLines, mLineIndex);
-
-    sdp = sdpLines.join('\r\n');
-    return sdp;
-  }
-
-  function extractSdp(sdpLine, pattern) {
-    var result = sdpLine.match(pattern);
-    return (result && result.length == 2) ? result[1] : null;
-  }
-
-  // Set the selected codec to the first in m line.
-  function setDefaultCodec(mLine, payload) {
-    var elements = mLine.split(' ');
-    var newLine = new Array();
-    var index = 0;
-    for (var i = 0; i < elements.length; i++) {
-      if (index === 3) // Format of media starts from the fourth.
-        newLine[index++] = payload; // Put target payload to the first.
-      if (elements[i] !== payload)
-        newLine[index++] = elements[i];
-    }
-    return newLine.join(' ');
-  }
-
-  // Strip CN from sdp before CN constraints is ready.
-  function removeCN(sdpLines, mLineIndex) {
-    var mLineElements = sdpLines[mLineIndex].split(' ');
-    // Scan from end for the convenience of removing an item.
-    for (var i = sdpLines.length - 1; i >= 0; i--) {
-      var payload = extractSdp(sdpLines[i], /a=rtpmap:(\d+) CN\/\d+/i);
-      if (payload) {
-        var cnPos = mLineElements.indexOf(payload);
-        if (cnPos !== -1) {
-          // Remove CN payload from m line.
-          mLineElements.splice(cnPos, 1);
-        }
-        // Remove CN line in sdp
-        sdpLines.splice(i, 1);
-      }
-    }
-
-    sdpLines[mLineIndex] = mLineElements.join(' ');
-    return sdpLines;
-  }
