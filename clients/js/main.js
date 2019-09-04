63b52a8c7634621e0e6677f543f510b57e6f8e52
gonnavis <gonnavis@163.com>
2019-09-04 12:54:04 +0800

    a

--- clients/js/main.js
+++ clients/js/main.js
@@ -3,7 +3,7 @@ var name;
 var connectedUser;
 
 //connecting to our signaling server 
-var conn = new WebSocket('ws://localhost:9090');
+var conn = new WebSocket(`ws://${location.hostname}:9090`);
 
 conn.onopen = function() {
   console.log("Connected to the signaling server");
