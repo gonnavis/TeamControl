868f4acc4cd8ebfd8095e0a9e93b259761b74ab4
gonnavis <gonnavis@163.com>
2019-10-12 18:55:36 +0800

    a

--- server/index.js
+++ server/index.js
@@ -32,100 +32,83 @@ wss.on('connection', function(connection) {
       data = {};
     }
 
-    //switching type of the user message 
-    switch (data.type) {
-      //when a user tries to login 
-      case "login":
-        console.log("User logged", data.name);
-        //if anyone is logged in with this username then refuse 
-        if (users[data.name]) {
-          sendTo(connection, {
-            type: "login",
-            success: false
-          });
-        } else {
-          //save user connection on the server 
-          users[data.name] = connection;
-          connection.name = data.name;
-
-          sendTo(connection, {
-            type: "login",
-            success: true
-          });
-        }
-
-        break;
-
-      case "offer":
-        //for ex. UserA wants to call UserB 
-        console.log("Sending offer to: ", data.name);
-
-        //if UserB exists then send him offer details 
-        var conn = users[data.name];
-
-        if (conn != null) {
-          //setting that UserA connected with UserB 
-          connection.otherName = data.name;
-
-          sendTo(conn, {
-            type: "offer",
-            offer: data.offer,
-            name: connection.name
-          });
-        }
-
-        break;
-
-      case "answer":
-        console.log("Sending answer to: ", data.name);
-        //for ex. UserB answers UserA 
-        var conn = users[data.name];
-
-        if (conn != null) {
-          connection.otherName = data.name;
-          sendTo(conn, {
-            type: "answer",
-            answer: data.answer
-          });
-        }
-
-        break;
-
-      case "candidate":
-        console.log("Sending candidate to:", data.name);
-        var conn = users[data.name];
-
-        if (conn != null) {
-          sendTo(conn, {
-            type: "candidate",
-            candidate: data.candidate
-          });
-        }
-
-        break;
-
-      case "leave":
-        console.log("Disconnecting from", data.name);
-        var conn = users[data.name];
-        conn.otherName = null;
-
-        //notify the other user so he can disconnect his peer connection 
-        if (conn != null) {
-          sendTo(conn, {
-            type: "leave"
-          });
-        }
-
-        break;
-
-      default:
+    //when a user tries to login 
+    if (data.type === "login") {
+      console.log("User logged", data.name);
+      //if anyone is logged in with this username then refuse 
+      if (users[data.name]) {
         sendTo(connection, {
-          type: "error",
-          message: "Command not found: " + data.type
+          type: "login",
+          success: false
         });
+      } else {
+        //save user connection on the server 
+        users[data.name] = connection;
+        connection.name = data.name;
 
-        break;
+        sendTo(connection, {
+          type: "login",
+          success: true
+        });
+      }
+    } else if (data.type === 'offer') {
 
+      //for ex. UserA wants to call UserB 
+      console.log("Sending offer to: ", data.name);
+
+      //if UserB exists then send him offer details 
+      var conn = users[data.name];
+
+      if (conn != null) {
+        //setting that UserA connected with UserB 
+        connection.otherName = data.name;
+
+        sendTo(conn, {
+          type: "offer",
+          offer: data.offer,
+          name: connection.name
+        });
+      }
+
+    } else if (data.type === 'answer') {
+      console.log("Sending answer to: ", data.name);
+      //for ex. UserB answers UserA 
+      var conn = users[data.name];
+
+      if (conn != null) {
+        connection.otherName = data.name;
+        sendTo(conn, {
+          type: "answer",
+          answer: data.answer
+        });
+      }
+
+    } else if (data.type === 'candidate') {
+      console.log("Sending candidate to:", data.name);
+      var conn = users[data.name];
+
+      if (conn != null) {
+        sendTo(conn, {
+          type: "candidate",
+          candidate: data.candidate
+        });
+      }
+    } else if (data.type === 'leave') {
+      console.log("Disconnecting from", data.name);
+      var conn = users[data.name];
+      conn.otherName = null;
+
+      //notify the other user so he can disconnect his peer connection 
+      if (conn != null) {
+        sendTo(conn, {
+          type: "leave"
+        });
+      }
+    } else {
+      sendTo(connection, {
+        type: "error",
+        message: "Command not found: " + data.type
+      });
     }
   });
 
